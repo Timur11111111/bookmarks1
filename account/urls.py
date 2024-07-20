52d0f271693d2a98781c7a9311e0c361c24150e7
Timur11111111 <newmew@tutamail.com>
2024-07-20 18:57:03 +0500

    author2

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .views import user_login, dashboard
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # path('login/', user_login, name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', dashboard, name='dashboard'),
 ]
