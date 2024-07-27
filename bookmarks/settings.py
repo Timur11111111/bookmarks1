cc6f20c85ddb57f40f2ab69f5c3dc068c4f2dbe0
Timur11111111 <newmew@tutamail.com>
2024-07-27 18:54:00 +0500

    login_email

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+        'django.contrib.auth.backends.ModelBackend',
+        'account.authentication.EmailAuthBackend',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
