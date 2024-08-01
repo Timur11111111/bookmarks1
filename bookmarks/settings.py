4d3a4afb8ea525dc190cf1578aa4216419c23dc7
Timur11111111 <newmew@tutamail.com>
2024-08-01 18:39:46 +0500

    images

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'account',
     'social_django',
     'django_extensions',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -163,6 +164,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
