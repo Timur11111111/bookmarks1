cc64746fececed65ca9ad17702275b0d720715fe
Timur11111111 <newmew@tutamail.com>
2024-08-01 16:38:27 +0500

    aaaa

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/victory/Рабочий стол/!python trainin/bookmarks/venv"
+set -gx VIRTUAL_ENV "/home/itpark/Документы/bookmarks1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
